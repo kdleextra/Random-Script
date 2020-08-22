7d05d91126ed2032384e696bb9b284af1503e3b2
kdleextra <61977296+kdleextra@users.noreply.github.com>
2020-08-22 17:44:09 -0500

    Add comment, add more input handles
    generalize the mainline function.

--- manga.py
+++ manga.py
@@ -1,12 +1,15 @@
-details = open('details.json','w')
-
-result = '{\n'
-
+# determine what kind of input -> handle it accordingly
 def getInput(kind):
-	print('>>' + kind +':')
-	string = input()
-	return '"'+ kind + '": "' + string + '",\n'
+	if kind == 'genre':
+		return getGenre()
+	elif kind == 'description':
+		return getDescription(kind)
+	else:
+		print('>>' + kind +':')
+		string = input()
+		return '"'+ kind + '": "' + string + '",\n'
 
+# function to split genre string and return the correct form
 def getGenre():
 	print('>> genre:')
 	s = input()
@@ -17,18 +20,25 @@ def getGenre():
 	genre ='"genre": ' + genre[:-1] + "],\n"
 	return genre
 
-
-
+# handle description with "quote" sign that break the details.json
+def getDescription(kind):
+	print('>> description:')
+	s = input()
+	s = s.replace('"','\'')
+	return '"'+ kind + '": "' + s + '",\n'
 
 ## Main Program ##
+details = open('details.json','w')
+result = '{\n'
 result += getInput('title')
 result += getInput('author')
 result += getInput('artist')
 result += getInput('description')
-result += getGenre()
+result += getInput('genre')
 print('\nstatus values": ["0 = Unknown", "1 = Ongoing", "2 = Completed", "3 = Licensed"]')
 result += getInput('status')
 result += '"_status values": ["0 = Unknown", "1 = Ongoing", "2 = Completed", "3 = Licensed"]\n}'
 details.write(result)
+details.close()
 
 
